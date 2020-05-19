26c70da68e517acecc8810d71cdd8f6b963f76d7
dkabot <1316469+dkabot@users.noreply.github.com>
2020-05-19 15:27:19 -0400

    Update references to discordapp.com in main.go

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func getOrigin(req *http.Request) string {
 	} else if strings.HasPrefix(req.Host, "ptb") {
 		return "ptb.discord.com"
 	} else {
-		return "discordapp.com"
+		return "discord.com"
 	}
 }
 
@@ -174,7 +174,7 @@ func modifyResponse(res *http.Response) error {
 				csp = strings.Replace(csp, "'self'", "'self' "+origin, -1)
 				// icons use wrong protocol
 				if res.Request.Header.Get("X-Forwarded-Proto") != "https" {
-					csp = strings.Replace(csp, "https://*.discordapp.com", "*.discordapp.com", -1)
+					csp = strings.Replace(csp, "https://*.discord.com", "*.discord.com", -1)
 					csp = strings.Replace(csp, "wss://*.discord.media", "ws://*.discord.media", -1)
 				}
 				v[i] = csp
